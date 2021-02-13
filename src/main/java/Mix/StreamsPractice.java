2795aec67e526ca9574568ccbc5c04b494437cbc
Gunjan Pandya <gunjanpandya@santoshs-air.lan>
2021-02-13 15:05:10 -0500

    streams

--- src/main/java/Mix/StreamsPractice.java
+++ src/main/java/Mix/StreamsPractice.java
@@ -1,6 +1,9 @@
 package Mix;
 
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -33,10 +36,28 @@ public class StreamsPractice {
 
     public static void main(String[] args) {
         StreamsPractice sp= new StreamsPractice();
+        List<String> stremspractice = new ArrayList<>();
+        stremspractice.add("abc");
+        stremspractice.add("bbb");
+        stremspractice.add("ccc");
+        stremspractice.add("abc");
+        stremspractice.add("eee");
+        stremspractice.add("fff");
+//        System.out.println(sp.fromArray1(new String[]{"g","n","u"}).count());
+//
+//       sp.wordsFlatMap(new String[]{"g j","n j","u k"});
 
-        System.out.println(sp.fromArray1(new String[]{"g","n","u"}).count());
+        System.out.println(stremspractice.stream().map(t -> t.getClass().toString()).toArray());
+        System.out.println(stremspractice.stream().filter(t -> t.equals("abc")).collect(Collectors.toList()));
+        System.out.println("Limited to 2");
+        stremspractice.stream().limit(2).forEach(t -> System.out.println(t));;
+        System.out.println("Display Distinct");
+        stremspractice.stream().distinct().forEach(System.out::println);
+        System.out.println("Display skip");
+        stremspractice.stream().skip(3).forEach(System.out::println);
 
-       sp.wordsFlatMap(new String[]{"g j","n j","u k"});
+       // System.out.println("Display sorted");
+       // stremspractice.stream().sorted(Comparator.comparing(stremspractice.get())).forEach(System.out::println);
     }
 
 
